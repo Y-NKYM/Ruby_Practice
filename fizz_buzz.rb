e364f8ac547bf87d03dc3429ca3ea07f0c9fff70
YukiNakayama <121099971+Y-NKYM@users.noreply.github.com>
2023-05-25 02:05:01 +0000

    [Finish]Ruby-8

---
+++ fizz_buzz.rb
@@ -0,0 +1,17 @@
+def fizz_buzz(number)
+
+  if (number%3 == 0) && (number%5 == 0)
+    return "FizzBuzz"
+  elsif number%3 == 0
+    return "Fizz"
+  elsif number%5 == 0
+    return "Buzz"
+  else
+    return "#{number}"
+  end
+end
+
+puts "数字を入力してください"
+number = gets.to_i
+puts "結果は…"
+puts fizz_buzz(number)
---
+++ greeting.rb
@@ -0,0 +1,10 @@
+def greeting(name)
+  return "Hello, #{name}!"
+  "Good morning, #{name}"
+end
+
+
+def calc(num)
+  return "計算できません" if num.zero?
+end
+puts calc(0)
